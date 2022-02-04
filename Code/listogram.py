0e9b07ecb345fe07ecff83db4da9355115d3c794
Jane <jane.anqi.cui@gmail.com>
2022-02-04 16:53:42 -0500

    listogram implemented

--- Code/listogram.py
+++ Code/listogram.py
@@ -21,25 +21,53 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        self.tokens += count 
+        word_found = False
+        for list in self: 
+            if list[0] == word: 
+                list[1] += count
+                word_found = True
+        if word_found == False: 
+            self.types += 1
+            self.append([word, count])
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for list in self: 
+            if list[0] == word: 
+                return list[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for list in self: 
+            if list[0] == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for index in range(len(self)): 
+            if self[index][0] == target:
+                return index
+        return None 
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        
+        random_index = random.randint(1, self.tokens)
 
+        goalpost = 0
+        for list in self: 
+            goalpost += list[1]
+            if goalpost >= random_index:
+                return list[0]
 
 def print_histogram(word_list):
     print()
